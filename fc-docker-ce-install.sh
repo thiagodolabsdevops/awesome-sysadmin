eb1669df8b7d101e00dfff76c8c289cd0ab489d7
Thiago Magalhaes <tmagalhaes1985@users.noreply.github.com>
2019-05-06 15:49:16 -0300

    Update fc-docker-ce-install.sh

--- fc-docker-ce-install.sh
+++ fc-docker-ce-install.sh
@@ -1,11 +1,16 @@
+# !/bin/bash
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+
 # Install Docker dependencies and repositories
-dnf -y install dnf-plugins-core
-dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-dnf -y install docker-ce docker-ce-cli containerd.io
+# dnf -y install dnf-plugins-core
+# dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+# dnf -y install docker-ce docker-ce-cli containerd.io
 
 # Enable and start Docker service
-systemctl enable docker
-systemctl start docker
+sudo systemctl enable docker
+sudo systemctl start docker
 
 # Add your user to the "docker" group to use Docker as a non-root user
 # usermod -aG docker your-user
